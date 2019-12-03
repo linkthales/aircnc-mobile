b7fa26219501df72f35b1d1a8d143d07113fafb3
Thales Martins <linkthales@hotmail.com>
2019-12-02 23:55:42 -0300

    Added versioning to aircnc-mobile from 9th Omnistack week.

---
+++ src/pages/List.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+import socketio from 'socket.io-client';
+import {SafeAreaView, ScrollView, Image, StyleSheet, Alert} from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.100.17:3333', {
+        query: {user_id},
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? 'APROVADA' : 'REJEITADA'
+          }`,
+        );
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    alignSelf: 'center',
+
+    resizeMode: 'contain',
+    height: 32,
+    marginTop: 10,
+  },
+});
